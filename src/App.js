f57f7fde5acd36a399dc55cf1d9747e5a0ef6570
Supriyays <supriya3523@gmail.com>
2021-02-11 18:30:49 +0530

    first new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import React, {useState} from 'react';
 
 const App = () => {
+  let [state, setState]=useState({
+      addtask: '',
+      isClick: false,
+      taskList: [],
+      checkbox: false
+    });
+    const addNewInput =(event) =>{
+      const val =event.target.value;
+      setState({...state, addtask: val});
+    }
+    const onSubmit = () => {
+      setState({...state, isClick:true, taskList: [...state.taskList, state.addtask]});
+      console.log(state.addtask, state.isClick);
+    }
+    const handleChangeChk = (i) => {
+       state.taskList.map((index) => {
+       if(index === i){
+         setState({...state, checkbox: true});
+       }})
+    }
   return (
-    <div>
-    <h1>Home Page</h1>
+    <div className="main">
+    <input type="text" placehoder="add new task" onChange={(event) => addNewInput(event)}/>
+    <button onClick={onSubmit}>Submit</button>
+    {state.isClick ? (
+      state.taskList.map((ele, index) => {
+      console.log('ele', ele);
+      return (
+      <div key={index} className="list">
+      <input
+      type="checkbox"
+      defaultChecked={state.checkbox}
+      onChange={() => handleChangeChk(index)}
+      />
+      {ele}
+      </div>
+      )
+      })
+    ) : null}
     </div>
   );
 }
